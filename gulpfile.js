b79c34437522fb97e9ea26d4733543fe606a394e
lupengyu <lupengyu@baidu.com>
2016-08-30 18:03:05 +0800

    添加 sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,15 @@ const clean = require('gulp-clean');
 const babelOptions = require('./package.json').babelBuild || {};
 const babelHelpers = require('gulp-babel-external-helpers');
 
+const sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('babel', function () {
     return gulp.src('src/**/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel(babelOptions))
         .pipe(babelHelpers('babelHelpers.js', 'umd'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('lib'));
-
 });
 
 gulp.task('stylus', function () {
